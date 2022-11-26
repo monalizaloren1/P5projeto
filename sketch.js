960c8fc88c11935a5cbb747da0c86d151f959c3d
monalizaloren1 <99450087+monalizaloren1@users.noreply.github.com>
2022-11-26 13:06:35 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function preload() {
+    img = loadImage('https://images.pexels.com/photos/128756/pexels-photo-128756.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+
+}
+function setup() {
+  createCanvas(???, ???);
+}
+
+function draw() {
+  image(img, 10, 10 ,150 ,120);
+  
+  fill(0, ???, 0);
+  stroke(0, 128, 0);
+  circle(350, 50, 70);
+
+}
